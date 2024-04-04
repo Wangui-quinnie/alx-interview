9ce65a7a360c26a281d602bce9027f274a6dee71
Wangui-quinnie <w.wangui@ymail.com>
2024-04-04 12:53:56 -0700

    added a nother bit mask to check if the second bit of the byte is 1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,7 +10,8 @@ def validUTF8(data):
     """
     num_bytes = 0
 
-    mask = 1 << 7
+    mask1 = 1 << 7
+    mask2 = 1 << 6
 
     for byte in data:
         if num_bytes == 0:
@@ -23,7 +24,7 @@ def validUTF8(data):
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if not (byte & mask):
+            if not (byte & mask1 and not (byte & mask2)):
                 return False
 
         num_bytes -= 1
