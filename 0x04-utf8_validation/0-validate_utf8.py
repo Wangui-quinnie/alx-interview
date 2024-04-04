d6811abe62a7f0e270ab7663a972252fca12aeb2
Wangui-quinnie <w.wangui@ymail.com>
2024-04-04 02:12:21 -0700

    used a mask

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module 0-validate_utf8
+"""
+
+
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
@@ -6,18 +11,16 @@ def validUTF8(data):
     num_bytes = 0
 
     for byte in data:
+        mask = 10000000
+        while mask & byte:
+            num_bytes += 1
+            mask >>= 1
+
         if num_bytes == 0:
-            if byte >> 5 == 0b110:
-                num_bytes = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3
-            elif byte >> 7:
-                return False
-        else:
-            if byte >> 6 != 0b10:
-                return False
-            num_bytes -= 1
+            continue
+        if num_bytes == 1 or num_bytes > 4:
+            return False
+
+        num_bytes -= 1
 
     return num_bytes == 0
