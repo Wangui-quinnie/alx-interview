4b55334e73518da4634fc414dfb23ad753ad4342
Wangui-quinnie <w.wangui@ymail.com>
2024-04-18 18:16:48 -0700

    fixed code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,29 @@
 #!/usr/bin/node
-
 const request = require('request');
+const ID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
 
-const movieId = process.argv[2];
-
-if (!movieId || isNaN(movieId)) {
-  console.error('Please provide a valid movie ID as the first argument.');
-  process.exit(1);
+function getCharacter (listChar, index) {
+  if (index === listChar.length) {
+    return;
+  }
+  request(`${listChar[index]}`, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const character = JSON.parse(response.body);
+      console.log(character.name);
+      getCharacter(listChar, index + 1);
+    }
+  });
 }
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
-    console.error('Error fetching data from the Star Wars API:', error);
-    process.exit(1);
+    console.log(error);
+  } else {
+    const film = JSON.parse(response.body);
+    const listChar = film.characters;
+    getCharacter(listChar, 0);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  const filmData = JSON.parse(body);
-
-  console.log('Characters in the movie:');
-  filmData.characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character data:', charError);
-        return;
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Unexpected status code:', charResponse.statusCode);
-        return;
-      }
-
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
-  });
 });
